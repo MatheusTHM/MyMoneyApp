f528936c0150b3915bc326cb18d322ada543c2e6
MatheusTHM <thommesani@gmail.com>
2021-03-26 20:53:16 -0300

    3/5

---
+++ frontend/src/common/tab/TabsHeader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TabsHeader = ({ children }) => {
+  return <ul className="nav nav-tabs">{children}</ul>;
+};
+
+export default TabsHeader;
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import DashboardReducer from '../billingCycle/dashboard/dashboardReducer';
+import tabReducer from '../common/tab/tabReducer';
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
+  tab: tabReducer,
 });
 
 export default rootReducer;
